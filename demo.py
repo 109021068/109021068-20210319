9f207e5f01f311b95eb9abe3b3cf3cafd24b8c17
20210312-1 <109021068@live.asia.edu.tw>
2021-04-18 21:15:51 +0800

    1

--- demo.py
+++ demo.py
@@ -1,20 +1,21 @@
 import requests as reqs
-from bs4 import BeautifulSoup  
+#from bs4 import BeautifulSoup  
 
-r = reqs.get("http://210.70.80.21/~bs109021068/")
+r = reqs.get("http://210.70.80.21/~bs109021001")
 r.encoding = "utf8"
-if r.status_code == 200:
-    #print(r.text)
-    soup = BeautifulSoup(r.text, "lxml")
+#if r.status_code == 200:
+print(r.status_code)
+print(r.text)
+    #soup = BeautifulSoup(r.text, "lxml")
     #print(soup)
-    result1 = soup.find_all("li")
+    #result1 = soup.find_all("li")
     #rint(result1)
-    fp = open("out2.txt","w",encoding="utf8")
-    for val in result1:
-        text2 = val.text.replace('\n','')
-        text3 = text2.replace(' ','')
-        print(text3)
-        fp.write(text3+"\n")
-    fp.close()
-else:
-    print("no page")
+    #fp = open("out2.txt","w",encoding="utf8")
+    #for val in result1:
+        #text2 = val.text.replace('\n','')
+        #text3 = text2.replace(' ','')
+        #print(text3)
+        #fp.write(text3+"\n")
+    #fp.close()
+#else:
+    #print("no page")
